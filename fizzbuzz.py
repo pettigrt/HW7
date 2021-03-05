34fc3d9466c6e3b58d0f2d8b483eb0d05eae76ef
Tyler Pettigrew <pettigrt@oregonstate.edu>
2021-03-05 13:24:20 -0800

    Implemented code for multiples of 3 and 5. test_fizzbuzz() now passes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,9 @@
 def fizzbuzz(x):
     if (x % 3 == 0):
-        return "Fizz"
+        if (x % 5 == 0):
+            return('FizzBuzz')
+        else:
+            return "Fizz"
     elif (x % 5 == 0):
         return "Buzz"
 
