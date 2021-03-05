e4f5d902aecc5dd8ae0912f4a36f927bc473bd2f
Tyler Pettigrew <pettigrt@oregonstate.edu>
2021-03-05 13:19:06 -0800

    Added second test: test_buzz asserts multiples of 5 returns buzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,6 +4,10 @@ import fizzbuzz
 class TestFizzBuzz(unittest.TestCase):
     def test_fizz(self):
         self.assertEqual(fizzbuzz.fizzbuzz(9), "Fizz")
+        
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz.fizzbuzz(10), "Buzz")
+
 
 
 if __name__ == "__main__":
